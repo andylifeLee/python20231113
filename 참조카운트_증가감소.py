6563a91f6b8143b670210c3f05951529f463d36e
AndyLee <r50632@gmail.com>
2023-11-14 16:41:52 +0900

    231114일 마지막시간

--- 참조카운트_증가감소.py
+++ 참조카운트_증가감소.py
@@ -2,15 +2,16 @@ class MyClass:
     #초기화 메서드(초기화)
     def __init__(self, value):
         self.Value = value 
-        print("Class is created! Value = ", value)
-    #소멸자 메서드(정리작업)    
+        print("Instance is created! Value = ", value)
+    #소멸자 메서드(정리작업)
     def __del__(self):
-        print("Class is deleted!")
+        print("Instance is deleted!")
         
+#메모리관리 자동 
+d = MyClass(5)
+#del d 
 
-c = MyClass(5)
-#del d
-print("전체코드 실행 종료")
-#c_copy = c
-#del c 
-#del c_copy
+print("전체 코드 실행 종료")
+
+
+
