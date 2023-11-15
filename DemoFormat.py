0bac02b36bc6a5317a2d9d97f42615d4de2ff3ea
AndyLee <r50632@gmail.com>
2023-11-15 10:38:39 +0900

    3일차 2교시

--- DemoFormat.py
+++ DemoFormat.py
@@ -17,13 +17,45 @@ for x in range(1,6):
     print(x,"*",x,"=", str(x*x).zfill(5))
 
 
+# #파일 쓰기 
+# f = open("c:\\work\\test.txt", "wt", encoding="utf-8")
+# f.write("첫번째라인\n두번째라인\n세번째라인\n")
+# f.close() 
+
+# #파일 읽기 
+# f = open("c:\\work\\test.txt", "rt", encoding="utf-8")
+# print(f.read())
+# f.close() 
+
+
 #파일 쓰기 
-f = open("c:\\work\\test.txt", "wt", encoding="utf-8")
+f = open(r"c:\work\test.txt", "wt", encoding="utf-8")
 f.write("첫번째라인\n두번째라인\n세번째라인\n")
 f.close() 
 
-#파일 읽기 
-f = open("c:\\work\\test.txt", "rt", encoding="utf-8")
-print(f.read())
+#파일에 첨부(append, read, write)
+f = open(r"c:\work\test.txt", "a+", encoding="utf-8")
+f.write("첫번째라인\n두번째라인\n세번째라인\n")
 f.close() 
 
+# #파일 읽기 
+# f = open(r"c:\work\test.txt", "rt", encoding="utf-8")
+# print(f.read())
+# f.close() 
+
+
+#파일 읽기 
+f = open(r"c:\work\test.txt", "rt", encoding="utf-8")
+print(f.read())
+#다시 처음으로 리셋
+f.seek(0)
+result = f.readlines()
+for item in result:
+    print(item)
+
+#다시 처음
+f.seek(0)
+print(f.readline(), end="")
+print(f.readline(), end="")
+
+f.close() 
--- Person.py
+++ Person.py
@@ -11,3 +11,13 @@ p2 = Person()
 p1.name = "전우치"
 p1.print()
 p2.print()
+
+
+
+#서식 지정
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(15000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
